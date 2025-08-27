1ef888ea68924dd11105bee151c1955da89e2165
turkifyy <126617740+turkifyy@users.noreply.github.com>
2025-08-27 16:33:01 +0100

    Update setup_rotation.js

--- src/setup_rotation.js
+++ src/setup_rotation.js
@@ -20,7 +20,7 @@ function initializeFirebase() {
                 credential: admin.credential.cert({
                     projectId: process.env.FIREBASE_PROJECT_ID,
                     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-                    clientEmail: process.env.CLIENT_EMAIL
+                    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
                 })
             });
         }
